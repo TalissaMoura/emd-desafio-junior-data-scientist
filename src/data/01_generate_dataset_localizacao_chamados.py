f01971e9f5de8ef2e5a9efb092093fb32d141222
TalissaMoura <talissa.ayres@gmail.com>
2024-02-21 16:03:35 -0400

    add datasets scripts

---
+++ src/data/01_generate_dataset_localizacao_chamados.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import basedosdados as bd
+from datetime import datetime
+from pathlib import Path
+
+def load_data_chamado_1746_with_geolocation(project_id: str,dir_to_save: str = None,ref_date: str="2024-02-01") -> pd.DataFrame:
+    path_to_save = Path(dir_to_save)
+    query = f"""
+      SELECT t1.*,
+       t2.nome as nome_bairro,
+       t2.subprefeitura as subprefeitura_bairro,
+       t2.area as area_bairro,
+       t2.perimetro as perimetro_bairro,
+       t2.geometry as geometry_bairro
+    FROM `datario.administracao_servicos_publicos.chamado_1746` t1
+    LEFT JOIN `datario.dados_mestres.bairro` t2
+    ON t1.id_bairro = t2.id_bairro
+    WHERE data_particao='{ref_date}'
+    """
+    df = bd.read_sql(query,billing_project_id=project_id)
+
+    # CONVERT DATE COLUMNS TO DATETIME
+
+    date_cols = df.select_dtypes(include="dbdate")
+    for col in date_cols.columns:
+        df[col] = pd.to_datetime(date_cols[col],format="%Y-%m-%d")
+
+    if path_to_save:
+        if path_to_save.is_dir():
+            df.to_parquet(f"{path_to_save}/dataset_chamado_1746_with_geoloc_{ref_date}.parquet.gzip",compression="gzip")
+        else:
+            path_to_save.mkdir(parents=True)
+            df.to_parquet(f"{path_to_save}/dataset_chamado_1746_with_geoloc_{ref_date}.parquet.gzip",compression="gzip")
+    
+    else:
+        return df
+
+if __name__ == "__main__":
+    PROJ_ID = "teste-cientista-dados-jr-rj"
+    REF_DATE = "2023-04-01"
+    load_data_chamado_1746_with_geolocation(project_id=PROJ_ID,
+                                                    ref_date=REF_DATE,
+                                                    dir_to_save="../../datasets/raw")
+    
---
+++ src/data/02_generate_dataset_pert_sossego.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import basedosdados as bd
+from datetime import datetime
+from pathlib import Path
+
+def load_data_chamado_1746_sub_per_sossego(project_id: str,dir_to_save: str = None,
+                                           first_ref_date:str="2022-01-01",sec_ref_date:str="2022-02-01") -> pd.DataFrame:
+    path_to_save = Path(dir_to_save)
+    query = f"""
+    SELECT t1.*
+    FROM `datario.administracao_servicos_publicos.chamado_1746` t1
+    WHERE data_particao BETWEEN "{first_ref_date}" AND "{sec_ref_date}"
+    AND t1.id_subtipo = "5071"
+    """
+    df = bd.read_sql(query,billing_project_id=project_id)
+
+    # CONVERT DATE COLUMNS TO DATETIME
+
+    date_cols = df.select_dtypes(include="dbdate")
+    for col in date_cols.columns:
+        df[col] = pd.to_datetime(date_cols[col],format="%Y-%m-%d")
+
+    if path_to_save:
+        if path_to_save.is_dir():
+            df.to_parquet(f"{path_to_save}/dataset_chamado_1746_pert_sossego_{first_ref_date}-{sec_ref_date}.parquet.gzip",compression="gzip")
+        else:
+            path_to_save.mkdir(parents=True)
+            df.to_parquet(f"{path_to_save}/dataset_chamado_1746_pert_sossego_{first_ref_date}-{sec_ref_date}.parquet.gzip",compression="gzip")
+    
+    else:
+        return df
+
+if __name__ == "__main__":
+    PROJ_ID = "teste-cientista-dados-jr-rj"
+    FIRST_REF_DATE = "2022-01-01"
+    SEC_REF_DATE = "2023-12-01"
+    load_data_chamado_1746_sub_per_sossego(project_id=PROJ_ID,
+                                                   first_ref_date=FIRST_REF_DATE,
+                                                   sec_ref_date=SEC_REF_DATE,
+                                                   dir_to_save="../../datasets/raw")
+    
