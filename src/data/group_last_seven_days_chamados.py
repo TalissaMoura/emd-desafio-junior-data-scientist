8452a24d9aaf347c3dc8c266e622ea713a381aa3
TalissaMoura <talissa.ayres@gmail.com>
2024-02-25 17:26:48 -0400

    change script to be for seven days

--- src/data/group_last_seven_days_chamados.py
+++ src/data/group_last_seven_days_chamados.py
@@ -4,16 +4,18 @@ import basedosdados as bd
 import pandas as pd
 
 
-def load_last_five_days_data_chamados(
+def load_last_seven_days_data_chamados(
     project_id: str, dir_to_save: str = None, ref_date: str = "2024-02-01"
 ) -> pd.DataFrame:
-
-    path_to_save = Path(dir_to_save)
+    if dir_to_save:
+        path_to_save = Path(dir_to_save)
+    else:
+        path_to_save = dir_to_save
     query = f"""
     SELECT DATE(t1.data_inicio) as data_inicio,
            COUNT(*) as qtd_chamados
     FROM `datario.administracao_servicos_publicos.chamado_1746` t1
-    AND DATE(t1.data_inicio) BETWEEN DATE_SUB(DATE '{ref_date}',INTERVAL 5 DAY)
+    WHERE DATE(t1.data_inicio) BETWEEN DATE_SUB(DATE '{ref_date}',INTERVAL 6 DAY)
     AND DATE '{ref_date}'
     GROUP BY 1
     ORDER BY 1 DESC;
@@ -30,13 +32,13 @@ def load_last_five_days_data_chamados(
     if path_to_save:
         if path_to_save.is_dir():
             df.to_csv(
-                f"{path_to_save}/dataset_last_five_days_chamado1746_{ref_date}.csv",
+                f"{path_to_save}/dataset_last_seven_days_chamado1746_{ref_date}.csv",
                 
             )
         else:
             path_to_save.mkdir(parents=True)
             df.to_csv(
-                f"{path_to_save}/dataset_last_five_days_chamado1746_{ref_date}.csv",
+                f"{path_to_save}/dataset_last_seven_days_chamado1746_{ref_date}.csv",
                 
             )
 
@@ -47,6 +49,6 @@ def load_last_five_days_data_chamados(
 if __name__ == "__main__":
     PROJ_ID = "teste-cientista-dados-jr-rj"
     REF_DATE = "2023-01-12"
-    load_last_five_days_data_chamados(
+    load_last_seven_days_data_chamados(
         project_id=PROJ_ID, ref_date=REF_DATE, dir_to_save="../../datasets/raw"
     )
