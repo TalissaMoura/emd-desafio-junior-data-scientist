5a9d08268f82733fcb100ec06705e9d0f348a740
TalissaMoura <talissa.ayres@gmail.com>
2024-02-25 18:58:26 -0400

    make lint

--- src/plot/bar_plot.py
+++ src/plot/bar_plot.py
@@ -1,28 +1,28 @@
-import altair as alt
-import pandas as pd
-import matplotlib.pyplot as plt
-from typing import Optional,Dict,Any
+from typing import Any, Dict, Optional
 
-def bar_plot(dataframe:pd.DataFrame,
-             x:Optional[str|alt.X],
-             y:Optional[str|alt.Y],
-             encode_kw:Optional[None|Dict[str,Any]]=None,
-             mark_bar_kw:Optional[Dict[str,Any]|None]=None,
-             config_xaxis:Optional[None|Dict[str,Any]]=None,
-             config_yaxis:Optional[None|Dict[str,Any]]=None):
-    
+import altair as alt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def bar_plot(
+    dataframe: pd.DataFrame,
+    x: Optional[str | alt.X],
+    y: Optional[str | alt.Y],
+    encode_kw: Optional[None | Dict[str, Any]] = None,
+    mark_bar_kw: Optional[Dict[str, Any] | None] = None,
+    config_xaxis: Optional[None | Dict[str, Any]] = None,
+    config_yaxis: Optional[None | Dict[str, Any]] = None,
+):
+
     if encode_kw:
+        base_bar = alt.Chart(dataframe).encode(x=x, y=y, **encode_kw)
+    else:
         base_bar = alt.Chart(dataframe).encode(
             x=x,
             y=y,
-            **encode_kw
-            )
-    else:
-       base_bar = alt.Chart(dataframe).encode(
-            x=x,
-            y=y,
-            )
-    
+        )
+
     if config_xaxis:
         base_bar = base_bar.configure_axisX(**config_xaxis)
     elif config_yaxis:
@@ -32,4 +32,3 @@ def bar_plot(dataframe:pd.DataFrame,
         return base_bar.mark_bar(**mark_bar_kw)
     else:
         return base_bar.mark_bar()
-    
