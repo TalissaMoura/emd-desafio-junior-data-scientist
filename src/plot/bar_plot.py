9c4e83c6bdfeaab986e1c3efc5b8e58e0a46d3e8
TalissaMoura <talissa.ayres@gmail.com>
2024-02-25 17:27:20 -0400

    create plot lib

---
+++ src/plot/bar_plot.py
@@ -0,0 +1,35 @@
+import altair as alt
+import pandas as pd
+import matplotlib.pyplot as plt
+from typing import Optional,Dict,Any
+
+def bar_plot(dataframe:pd.DataFrame,
+             x:Optional[str|alt.X],
+             y:Optional[str|alt.Y],
+             encode_kw:Optional[None|Dict[str,Any]]=None,
+             mark_bar_kw:Optional[Dict[str,Any]|None]=None,
+             config_xaxis:Optional[None|Dict[str,Any]]=None,
+             config_yaxis:Optional[None|Dict[str,Any]]=None):
+    
+    if encode_kw:
+        base_bar = alt.Chart(dataframe).encode(
+            x=x,
+            y=y,
+            **encode_kw
+            )
+    else:
+       base_bar = alt.Chart(dataframe).encode(
+            x=x,
+            y=y,
+            )
+    
+    if config_xaxis:
+        base_bar = base_bar.configure_axisX(**config_xaxis)
+    elif config_yaxis:
+        base_bar = base_bar.configure_axisY(**config_yaxis)
+
+    if mark_bar_kw:
+        return base_bar.mark_bar(**mark_bar_kw)
+    else:
+        return base_bar.mark_bar()
+    
